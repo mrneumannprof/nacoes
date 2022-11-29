908b078cb69d4fc98cffc9747804dc1e80c271f1
Mariana Ribinski Neumann <104335598+mrneumannprof@users.noreply.github.com>
2022-11-29 00:24:42 -0300

    teste2

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function monstrarImagem(img) {
+    const imagemGrande=document.getElementById("imagemGrande");
+    imagemGrande.scr=img.src;
+    imagemGrande.parentElement.style.display="block";
+    
+}
