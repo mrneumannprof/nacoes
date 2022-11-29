7c06f8abc3acc8508afaf3b43b4ea1f52480cf75
Mariana Ribinski Neumann <104335598+mrneumannprof@users.noreply.github.com>
2022-11-29 11:02:05 -0300

    js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-function monstrarImagem(img) {
-    const imagemGrande=document.getElementById("imagemGrande");
-    imagemGrande.scr=img.src;
-    imagemGrande.parentElement.style.display="block";
-}
